90067d67f27fbcc2c137a4bf48ab83686848911d
Thanat Chokwijitkul <alexenriquent@gmail.com>
2015-08-28 22:11:05 +1000

    Amending rendering performance

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
+var http = require('http');
 var express = require('express');
+var ejs = require('ejs');
 var app = express();
 var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/public'));
+app.engine('html', ejs.renderFile);
 
 app.get('/', function(req, res) {
-	res.sendFile('index.html');
+	res.render('index.html', { });
 });
 
-app.listen(port);
-
-console.log('Listening on port ' + port);
+http.createServer(app).listen(port, function() {
+	console.log('Listening on port ' + port);
+});
