b179432397fa43025c3b433199ffd03de1c6261b
Thanat Chokwijitkul <alexenriquent@gmail.com>
2015-08-28 17:01:43 +1000

    Adding places search box

--- public/javascripts/map-script.js
+++ public/javascripts/map-script.js
@@ -12,8 +12,11 @@ function initMap() {
             center: pos,
             zoom: 16
         });
+
+        placesSearch();
+
         infowindow = new google.maps.InfoWindow();
-		var type = '';
+		var type = null;
 		searchNearby(type);
     }, function() {
         handleLocationError(true, infowindow, map.getCenter());
@@ -25,19 +28,18 @@ function initMap() {
 
 function searchNearby(type) {
 	var service = new google.maps.places.PlacesService(map);
-	service.nearbySearch({
-		location: pos,
-		radius: 1000,
-		types: [type]
-	}, callback);
+	if (type != null) {
+		service.nearbySearch({
+			location: pos,
+			radius: 1000,
+			types: [type]
+		}, callback);
+	}
 }
 
 function callback(results, status) {
 	if (status === google.maps.places.PlacesServiceStatus.OK) {
-		for (var i = 0; i < markers.length; i++) {
-			markers[i].setMap(null);
-		}
-		markers = [];
+		clearMarkers();
 		for (var i = 0; i < results.length; i++) {
 			createMarker(results[i]);
 		}
@@ -59,6 +61,51 @@ function createMarker(place) {
 	markers.push(marker);
 }
 
+function clearMarkers() {
+	for (var i = 0; i < markers.length; i++) {
+			markers[i].setMap(null);
+		}
+		markers = [];
+}
+
+function buttonSearch(button, type) {
+	button.onclick = function() {
+		searchNearby(type);
+	};
+}
+
+function placesSearch() {
+	var input = document.getElementById('places-input');
+	var searchBox = new google.maps.places.SearchBox(input);
+	map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+	map.addListener('bounds_changed', function() {
+		searchBox.setBounds(map.getBounds());
+	});
+
+	searchBox.addListener('places_changed', function() {
+		var places = searchBox.getPlaces();
+
+		if (places.length == 0) {
+			return;
+		}
+
+		clearMarkers();
+
+		var bounds = new google.maps.LatLngBounds();
+		places.forEach(function(place) {
+			createMarker(place);
+
+			if (place.geometry.viewport) {
+				bounds.union(place.geometry.viewport);
+			} else {
+				bounds.extend(place.geometry.location);
+			}
+		});
+		map.fitBounds(bounds);
+	});
+}
+
 window.onload = function() {
 	var types = ['restaurant', 'cafe', 'bar', 'night_club', 
 				'meal_delivery', 'meal_takeaway', 'bakery', 'liquor_store'];
@@ -68,11 +115,5 @@ window.onload = function() {
 	for (var i = 0; i < 10; i++) {
 		buttonSearch(buttons[i], types[i]);
 	}
-}
-
-function buttonSearch(button, type) {
-	button.onclick = function() {
-		searchNearby(type);
-	};
 }
 
