322d3e9c0c7276c90c056adc94471943c8b56a74
Thanat Chokwijitkul <alexenriquent@gmail.com>
2015-09-03 10:35:57 +1000

    Encapsulated server

---
+++ app.js
@@ -0,0 +1,53 @@
+// Import modules
+var express = require('express');
+var ejs = require('ejs');
+var logger = require('morgan');
+
+// Import a module from 'routes'
+var routes = require('./routes/index');
+
+// Create an 'express' object
+var app = express();
+
+// Use Morgan - Log requests to the terminal console
+app.use(logger('dev'));
+// Add connection to the 'views' folder for css and javascipt
+app.use(express.static(__dirname + '/views'));
+// Add connection to the 'public' folder for html
+app.use(express.static(__dirname + '/public'));
+// Set EJS as a templating language with HTML as an extension
+app.engine('.html', ejs.__express);
+app.set('view engine', 'html');
+
+// Route for 'index.html'
+app.get('/', routes.index);
+
+// Catch 404 and forward to rror handler
+app.use(function(req, res, next) {
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+// Development error handler
+if (app.get('env') === 'development') {
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
+}
+
+// Production error handler
+app.use(function(err, req, res, next) {
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
+});
+
+// Export 'app' module
+module.exports = app;
--- server.js
+++ server.js
@@ -1,57 +1,9 @@
 // Import modules
 var http = require('http');
-var express = require('express');
-var ejs = require('ejs');
-var logger = require('morgan');
-
-// Import a module from 'routes'
-var routes = require('./routes/index');
-
-// Create an 'express' object
-var app = express();
+var app = require('./app');
 
 // Identify port
 var port = process.env.PORT || 8080;
-
-// Use Morgan - Log requests to the terminal console
-app.use(logger('dev'));
-// Add connection to the 'views' folder for css and javascipt
-app.use(express.static(__dirname + '/views'));
-// Add connection to the 'public' folder for html
-app.use(express.static(__dirname + '/public'));
-// Set EJS as a templating language with HTML as an extension
-app.engine('.html', ejs.__express);
-app.set('view engine', 'html');
-
-// Route for 'index.html'
-app.get('/', routes.index);
-
-// Catch 404 and forward to rror handler
-app.use(function(req, res, next) {
-	var err = new Error('Not Found');
-	err.status = 404;
-	next(err);
-});
-
-// Development error handler
-if (app.get('env') === 'development') {
-	app.use(function(err, req, res, next) {
-		res.status(err.status || 500);
-		res.render('error', {
-			message: err.message,
-			error: err
-		});
-	});
-}
-
-// Production error handler
-app.use(function(err, req, res, next) {
-	res.status(err.status || 500);
-	res.render('error', {
-		message: err.message,
-		error: {}
-	});
-});
 
 // Create a server and log a message to the console
 http.createServer(app).listen(port, function() {
